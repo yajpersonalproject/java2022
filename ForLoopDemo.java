0e2c2ee2c15c8e8c4666c75f9b4caf4b3e795092
teacher Marcos <teacher.mrdv@gmail.com>
2022-10-28 12:35:03 +0800

    Create ForLoopDemo.java

---
+++ ForLoopDemo.java
@@ -0,0 +1,22 @@
+
+public class ForLoopDemo
+{
+	
+	public static void main (String[] args)
+	{
+		for(int i = 0; i < 11; i++)
+		{	System.out.print(i + " ");
+		}
+		System.out.println();
+		System.out.println("---------------");
+		for(int i = 10; i >= 0; i--)
+		{	System.out.print(i + " ");
+		}
+		System.out.println();
+		System.out.println("---------------");
+		for(int i = 0; i < 21; i=i+2)
+		{	System.out.print(i + " ");
+		}
+	}
+}
+
