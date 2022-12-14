cf7f9e42051367d46908a4da7c8745eea1462264
teacher Marcos <teacher.mrdv@gmail.com>
2022-12-14 16:00:37 +0800

    Update Chapter6Problem3.java

--- Workhseets/answers/Chapter6Problem3.java
+++ Workhseets/answers/Chapter6Problem3.java
@@ -38,7 +38,7 @@ public class Chapter6Problem3
 		{	// lines 39 is 6.3
 			System.out.println( spaces(n-i) + stars(n)
 			+ spaces(i*2) + stars(n) );
-			// line 40 completes the top of the diamond
+			// line 40 completes the top-right side of the diamond
 		}
 		
 		for(int i = 1; i <= n; i++)
