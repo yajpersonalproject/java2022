c8a9bf6f52b56fcb721116fc4b8c308a57cec0e3
teacher Marcos <teacher.mrdv@gmail.com>
2022-12-14 10:54:41 +0800

    Create XmasTree2.java
    with method implementation

---
+++ Demos/XmasTree2.java
@@ -0,0 +1,54 @@
+/*
+ * This class will output a Xmas tree
+ * 
+ */
+ 
+public class XmasTree2
+{
+
+	// this method makes a String of <howMany> asterisks ( a line of <howMany> *s )
+	static String stars(int howMany)
+	{
+		String output = ""; // empty string
+		for(int i = 0; i < howMany; i++)
+		{
+			output = output + "*";
+		}
+		return output;
+	}
+
+	// this method makes a String of <howMany> spaces
+	static String spaces(int howMany)
+	{
+		String output = ""; // empty string
+		for(int i = 0; i < howMany; i++)
+		{
+			output = output + " ";
+		}
+		return output;
+	}
+
+	static void XmasTree(int size)
+	{
+		for(int i = 1; i <= size; i++)
+		{
+			System.out.println( spaces(size-i) +
+				stars(i) + stars(i-1) );
+		}
+		for(int i = 1; i <= size/2; i++)
+		{
+			System.out.println( spaces(size-1) +
+				"*" );
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		int s = IBIO.inputInt("Xmas tree size? ");
+		System.out.println( "\n" );
+		XmasTree(s);
+	}
+	
+	
+}
+
