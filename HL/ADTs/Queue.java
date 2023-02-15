3a6dfa28a63d3cd3f23ff42ef683a5fbf2768d7c
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2023-02-16 02:03:21 +0800

    Queue challenge files

---
+++ HL/ADTs/Queue.java
@@ -0,0 +1,37 @@
+//
+// ADT: queue skeleton -- FIFO
+// enqueue, dequeue, isEmpty, printQueue
+//
+public class Queue
+{
+	Node head;
+	Node tail;
+	
+	public boolean isEmpty()
+	{
+		return false; // change as appropriate
+	}
+	
+	public void enQueue(int data)
+	{
+		Node newNode = new Node();
+		newNode.data = data;
+		// change as appropriate
+		
+	}
+	
+	public int deQueue()
+	{	
+		// change as appropriate
+		return 0;
+		
+	}
+	
+	public void printQueue()
+	{	
+		// add as appropriate
+	}
+			
+	
+}
+
