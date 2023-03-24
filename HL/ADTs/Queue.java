59e8408539555b86217a1d9c6c89ac74cb72c0a0
teacher Marcos <teacher.mrdv@gmail.com>
2023-03-24 16:23:46 +0800

    Friday update

--- HL/ADTs/Queue.java
+++ HL/ADTs/Queue.java
@@ -11,6 +11,7 @@ public class Queue
 {
     Node head;
     Node tail;
+    int size = 0;
 
     public boolean isEmpty()
     {
@@ -20,19 +21,57 @@ public class Queue
 
     public void enQueue(int data)
     {
-
+        Node newNode = new Node(data);
+        if( isEmpty() )
+        {
+            head = newNode;
+        } else
+        {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
     }
 
     public int deQueue()
     {
-        return 0;
+        if (!isEmpty())
+        {
+            int d = head.data;
+            head  = head.next;
+            size--;
+            return d;
+        } else
+        {
+            System.out.println("Queue is empty.");
+            return 0;
+        }
     }
 
     public void printQueue()
     {
-        
+        Node temp = head;
+        System.out.print("head -> ");
+        while (temp != null)
+        {
+            if(temp.next == null) System.out.print("tail -> ");
+            System.out.print(temp.data);
+            System.out.print(" -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL. Size = " + size);
+    }
+
+    public int size()
+    {
+        Node temp = head;
+        int counter = 0;
+        while (temp != null)
+        {
+            counter++;
+            temp = temp.next;
+        }
+        return counter;
     }
 
 }
-
-
