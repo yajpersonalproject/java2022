0d3f7617f43dc80f3761acc3285feb6cdff66109
teacher Marcos <teacher.mrdv@gmail.com>
2023-03-24 10:03:44 +0800

    Fri 24.3 update

--- WorkSheets2022/src/Chapter10Problem2.java
+++ WorkSheets2022/src/Chapter10Problem2.java
@@ -24,7 +24,7 @@ public class Chapter10Problem2
     static int power(int base, int exp)
     {
         if (exp == 0)
-            return base;
+            return 1;
         int power = 1;
         for (int i = 0; i < exp; i++)
         {
@@ -33,31 +33,46 @@ public class Chapter10Problem2
         return power;
     }
 
-
     static int bin2int(String binary)
     {
         char[] binArray = binary.toCharArray();
-        int[] powersOf2 = new int[binArray.length+1];
-
-        for (int i = binArray.length; i >= 0; i--)
+        int[] binValues = new int[binArray.length]; // or binary.length()
+        int[] powersOf2 = new int[binArray.length]; // or binary.length()
+        // first we build an array with the powers of 2, for the positional
+        // notation approach
+        for (int i = 0; i < binArray.length; i++)
         {
-            powersOf2[i] = power(2, i);
+            powersOf2[binArray.length-1-i] = power(2, i);
+        }
+        // second, we convert the char array into an int array (binValues)
+        for(int i = 0; i < binArray.length; i++)
+        {
+            if (binArray[i] == '1')
+                binValues[i] = 1;
+            else if(binArray[i] == '0')
+                binValues[i] = 0;
         }
         System.out.println(Arrays.toString(powersOf2));
-        return 0;
+        System.out.println(Arrays.toString(binValues));
+        int total = 0;
+        for(int i = 0; i < binValues.length; i++)
+        {
+            total = total + (binValues[i] * powersOf2[i]);
+        }
+        return total;
     }
 
     public static void main(String[] args)
     {
-        //System.out.println(power(2,8));
-        System.out.println(bin2int("1010"));
+        //System.out.println(power(2,8)); //testing
+        //System.out.println(bin2int("1010")); //testing
         String binary = IBIO.input("Enter a binary number: ");
         if (!isBinary(binary))
         {
             System.out.println("Error - input is not a binary number");
         } else
         {
-
+            System.out.println(bin2int(binary));
         }
     }
 
