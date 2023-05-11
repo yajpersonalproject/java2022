35d01ef8dcdcc8867c322625d6d3fdb5c24b5e2f
teacher Marcos <teacher.mrdv@gmail.com>
2023-05-11 10:41:40 +0800

    Thursday update

--- assignments/examPrep/S2ExamPrep2.java
+++ assignments/examPrep/S2ExamPrep2.java
@@ -42,7 +42,7 @@ public class S2ExamPrep2
 	
 	public static int largest(int[] array)
 	{
-		int max = primes[primes.length-1];
+		int max = primes[0];  // this assumes that the primes array is sorted in ascending order, otherwise you'd have to find the smallest prime in the primes array first
 		for(int i = 1; i < array.length; i++)
 		{
 			for(int j = 0; j < primes.length; j++)
@@ -54,12 +54,22 @@ public class S2ExamPrep2
 		}
 		return max;
 	}
-/*	
+
 	public static int smallest(int[] array)
 	{
-		
+		int min = primes[primes.length-1];  // this assumes that the primes array is sorted in ascending order, otherwise you'd have to find the largest prime in the primes array first
+		for(int i = 1; i < array.length; i++)
+		{
+			for(int j = 0; j < primes.length; j++)
+			{
+				if(array[i] == primes[j])
+					if(array[i] < min)
+						min = array[i];
+			}
+		}
+		return min;
 	}
-	
+/* for you to try!	
 	public static double average(int[] array)
 	{
 		
@@ -69,12 +79,11 @@ public class S2ExamPrep2
 	{
 		int[] random = { 8, 6, 1, 9, 3, 6, 6, 8, 8, 8, 7, 9, 2, 5, 6, 7, 6, 6, 6, 7, 7, 8, 3, 3, 5, 5, 5, 8, 3, 5, 9, 4, 3, 7, 9, 9, 4, 7, 8, 3, 1, 9, 4, 6, 1, 1, 8, 2, 5, 3, 6, 9, 7, 1, 8, 3, 2, 8, 3, 8, 1, 9, 3, 3, 3, 8, 8, 4, 6, 4, 7, 5, 6, 6, 3, 2, 2, 2, 4, 7, 6, 2, 6, 3, 8, 5, 8, 3, 9, 9, 4, 8, 3, 6, 6, 4, 1, 3, 8, 6, 1, 1, 8, 3, 2, 9, 9, 2, 5, 5, 1, 3, 1, 9, 2, 4, 3, 3, 2, 2, 1, 3, 8, 4, 8, 2, 8, 1, 2, 8, 2, 5, 5, 4, 7, 6, 4, 1, 7, 4, 5, 1, 1, 9, 3, 8, 3, 9, 4, 2, 4, 7, 9, 9, 4, 2, 3, 4, 5, 8, 6, 9, 6, 6, 6, 5, 4, 7, 1, 4, 7, 2, 1, 6, 9, 1, 6, 7, 5, 4, 4, 1, 4, 2, 4, 2, 4, 5, 3, 9, 6, 6, 5, 5, 5, 1, 1, 7, 8, 5 };
 		
-		System.out.println( random.length );
-		System.out.println( count(random) );
-		System.out.println( largest(random) );
-		//System.out.println( smallest(random) );
-		//System.out.println( average(random) );
-		
+		System.out.println( "Random numbers array length = " + random.length );
+		System.out.println( "How many single digit primes? = " + count(random) );
+		System.out.println( "Largest prime in array = " + largest(random) );
+		System.out.println( "Smallest prime in array = " + smallest(random) );
+		//System.out.println( "Average of primes in array = " + average(random) );
 	}
 }
 
