791d050bcfff4e839492f43c357d61584e416201
teacher Marcos <teacher.mrdv@gmail.com>
2023-08-25 11:39:09 +0800

    Update RabbitTest.java

--- OOP/Zoologico/src/RabbitTest.java
+++ OOP/Zoologico/src/RabbitTest.java
@@ -3,6 +3,7 @@ public class RabbitTest
     public static void main(String[] args)
     {
         System.out.println("Rabbit test class");
+        // complete this class -- use AnimalExample and ZebraTest as inspiration
 
     }
 }
